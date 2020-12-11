9aa8fb42a86f1863d12c408160e5ea11bfc01c45
Jefferson Ferreira Felizardo <jefferson_ferreira@pmedico.com>
2020-12-11 08:11:18 -0300

    Iniciando

---
+++ index.js
@@ -0,0 +1,36 @@
+const prova = {
+  disciplina: 'SCA ( saúde da criança e do adolesnte) III',
+  questoes: {
+    enunciados: [
+      'A constipação intestinal é um sintoma frequente em crianças, sendo responsável por cerca de 3% das consultas nos   ambulatórios da pediatria. Sobre o tema, assinale a alternativa correta:',
+      'Em relação às orientações terapêuticas para a constipação intestinal, considera-se que:',
+    ],
+    respostas: [
+      {
+        a: 'A maioria dos casos de constipação é de origem orgânica.',
+        b:
+          'A constipação é uma doença benigna e não traz complicações para a criança, havendo resolução do quadro com orientações dietéticas.',
+        c:
+          'Considera-se constipação crônica primária orgânica, quando há alterações estruturais do trato gastrointestinal.',
+        d:
+          'É denominada constipação crônica primária funcional simples quando existe o trânsito colônico lento, disfunção do assoalho pélvico ou sensibilidade anorretal diminuída.',
+        e:
+          'A presença de incontinência fecal retentiva exclui o diagnóstico de constipação funcional.',
+      },
+      {
+        a:
+          'Quando houver fecaloma ou impactação fecal, o esvaziamento do reto e do colo não é etapa fundamental, podendo ficar para um segundo momento do tratamento.',
+        b:
+          'Fazem parte do tratamento da constipação intestinal maior consumo de líquidos, aumento da atividade física e dieta pobre em fibra alimentar, em que metade dos cereais deve ser integral ou com farelo de trigo e os grãos e frutas ingeridos sem casca.',
+        c:
+          'A resposta clínica favorável no tratamento da constipação intestinal pode ser esperada com qualquer cepa de probiótico.',
+        d:
+          'Após melhora do quadro clínico, e havendo aceitação da dieta adequada, deve-se retirar o laxante progressivamente a fim de evitar o fracasso no tratamento com recidiva da impactação fecal, decorrente da utilização do laxante em dose baixa ou de forma não contínua.',
+        e: 'O óleo mineral é o medicamento de escolha para lactentes.',
+      },
+    ],
+  },
+};
+prova.map(prova => console.log(prova));
+// console.log(prova.questoes.enunciados[0]);
+// console.log(prova.questoes.respostas[0]);
